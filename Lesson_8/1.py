c7f1fe350d46f9583bc9a0f8c11b7879c9c5f12a
DerianQM <Kor_galom@mail.ru>
2019-03-31 22:18:25 +0300

    1е задание и немного гибридный код 2го задания на основе 2х примеров вебинара + анализ 2го задания, что смог, да, самому написать трудно код или придумать более рациональный

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,22 @@
-"""
-1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
-"""
+import hashlib
+
+
+def sub_s_count(s):
+
+    sub_s = set()
+
+    for i in range(len(s)-1):
+        for j in range(len(s), i, -1):
+            sub_s.add(
+                hashlib.sha1(s[i:j].encode('utf-8')).hexdigest())
+
+    return sub_s
+
+
+s = input('Введите строку из маленьких латинских букв: ')
+print(f'Строка  - {s}')
+
+for el in sub_s_count(s):
+    print(el)
+
+print(f'\nКоличество подстрок == {len(sub_s_count(s))}')
--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -1,5 +1,53 @@
-"""
-2*. Определение количества различных подстрок с использованием хэш-функции.
-Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
-Требуется найти количество различных подстрок в этой строке.
-"""
+from collections import Counter
+
+s = "happy three friends"
+
+print(f'Исходная строка  - {s}')
+
+
+class Node:
+    def __init__(self,left = None,right = None):
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        return f'Левая сторона [{self.left}] Правая сторона [{self.right}]'
+    def children(self):
+        return self.left, self.right
+
+def make_haffman_tree(node, code=''):
+
+    if type(node) is str:
+        return {
+            node:code
+        }
+
+    l,r = node.children()
+
+    result = {}
+
+    result.update(make_haffman_tree(l, code + '0')) #распихиваем 0 и 1
+    result.update(make_haffman_tree(r, code + '1'))
+    return result
+
+count_s = Counter(s)
+
+tree = count_s.items()
+
+while len(tree) >1: # строим дерево матрешкой, порядковую зависимось эл-тов, каждый раз срезая суммированный конец
+    tree = sorted(tree,reverse = True, key = lambda x:x[1])
+    print(tree)
+    char1,count1 = tree[-1]
+    char2,count2 = tree[-2]
+    tree = tree[:-2]
+    tree.append(
+        (Node(char1,char2), count1+count2)
+    )
+
+code_table = make_haffman_tree(tree[0][0])
+print(f'\nСформированная таблица кодов символов - {code_table}')
+coded = []
+
+for char in s:
+    coded.append(code_table[char])
+
+print('Закодированная строка: %s'%''.join(coded))
